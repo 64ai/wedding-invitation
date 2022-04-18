fb191bc8b5d157b206a0e1ea24cb4856c20b16e7
sam.rho(노승남)/kep <sam.rho@kakaoenterprise.com>
2022-04-18 21:27:20 +0900

    feat: map scroll 방지, zoom origin 고정

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -6,15 +6,18 @@ export type MapProps = {};
 
 const Map: FC<MapProps> = (props) => {
   useEffect(() => {
+    const lat = 35.1877130;
+    const lng = 129.2123966;
     const mapOptions = {
-      center: new naver.maps.LatLng(35.1877130,129.2123966),
+      center: new naver.maps.LatLng(lat, lng),
       zoom: 13,
       scaleControl: false,
       logoControl: false,
       mapDataControl: false,
-      // zoomControl: true,
       minZoom: 10,
-      maxZoom: 15,
+      maxZoom: 17,
+      draggable: false,
+      zoomOrigin: {lat, lng},
     };
 
     var map = new naver.maps.Map('_map', mapOptions);
@@ -22,10 +25,10 @@ const Map: FC<MapProps> = (props) => {
       map,
       position: map.getCenter(),
     });
-  },[]);
+  }, []);
 
   return (
-    <div 
+    <div
       style={{
         height: '100%',
         display: 'flex',
@@ -37,7 +40,9 @@ const Map: FC<MapProps> = (props) => {
       }}
       onClick={() => {
         const s = window.confirm('네이버 지도로  이동하시겠습니까?');
-
+        if (s) {
+          window.location.href = 'https://m.place.naver.com/restaurant/1646847275/home';
+        }
       }}
     >
       <div id="_map" style={{
