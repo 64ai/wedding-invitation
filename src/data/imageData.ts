f54e175a7779f02cdc55083ac2848d8aff6bb53b
sam.rho(노승남)/kep <sam.rho@kakaoenterprise.com>
2022-04-24 21:00:18 +0900

    resource: image order update

--- src/data/imageData.ts
+++ src/data/imageData.ts
@@ -14,6 +14,21 @@ export const imageData: ImageType[] = [
     y: 10,
   },
   {
+    src: '/images/6_-min.jpg',
+    alt: 'wedding 사진',
+    x: -183,
+    y: 10,
+  },
+  {
+    src: '/images/5_-need-resize-min.jpg',
+    alt: 'wedding 사진',
+    x: 0,
+    y: 10,
+    scale: 1.7,
+    translateX: 60,
+    translateY: 152,
+  },
+  {
     src: '/images/3_-min.jpg',
     alt: 'wedding 사진',
     x: -153,
@@ -27,21 +42,6 @@ export const imageData: ImageType[] = [
     y: 10,
   },
   {
-    src: '/images/5_-min.jpg',
-    alt: 'wedding 사진',
-    x: 0,
-    y: 10,
-    scale: 1.7,
-    translateX: 60,
-    translateY: 152,
-  },
-  {
-    src: '/images/6_-min.jpg',
-    alt: 'wedding 사진',
-    x: -183,
-    y: 10,
-  },
-  {
     src: '/images/7_-min.jpg',
     alt: 'wedding 사진',
     x: 0,
@@ -49,9 +49,21 @@ export const imageData: ImageType[] = [
     scale: 1.2,
   },
   {
-    src: '/images/8_-min.jpg',
+    src: '/images/IMG_7675-min.JPG',
     alt: 'wedding 사진',
     x: 0,
+    y: 10,
+  },
+  {
+    src: '/images/IMG_7692-min.JPG',
+    alt: 'wedding 사진',
+    x: 0,
+    y: 10,
+  },
+  {
+    src: '/images/IMG_7705-min.JPG',
+    alt: 'wedding 사진',
+    x: -152,
     y: 10,
   },
   {
